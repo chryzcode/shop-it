842e783c06f6434f46d1b9fb23aa966e24fd9a9c
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-02-18 14:12:29 +0100

    init

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from turtle import mode
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    # pulral for the table name in the admin page
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
