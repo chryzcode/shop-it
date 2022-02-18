f290ecc6bce9fa49e4a89ee51def2a4d1bd0d158
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-02-18 16:33:46 +0100

    push

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
-
+from django.utils.text import slugify
 from app.models import User, Category, Product
 
 class TestCategoryModel(TestCase):
     def test_category_model(self):
         category = Category.objects.create(name="test there")
         self.assertEqual(category.name, "test there")
-        self.assertEqual(category.slug, "test-there")
+        self.assertEqual(category.slug, slugify(category.name))
