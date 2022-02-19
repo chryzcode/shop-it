2f96c604b8232f3d523a62327b1fd2e5233324da
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-02-19 22:05:12 +0100

    commit

--- app/views.py
+++ app/views.py
@@ -4,12 +4,12 @@ from .models import Product, User, Category
 
 # Create your views here.
 
-def all_products(request, slugified_username):
+def a_user_all_products(request, slugified_username):
     user = get_object_or_404(User, slugified_username=slugified_username)
     all_products = Product.objects.filter(created_by=user.id)
     return render(request, 'app/all-products.html', {'all_products': all_products})
 
-def all_categories(request, slugified_username):
+def a_user_all_categories(request, slugified_username):
     user = get_object_or_404(User, slugified_username=slugified_username)
     all_categories = Category.objects.filter(created_by=user.id)
     return render(request, 'app/all-categories.html', {'all_categories': all_categories})
@@ -19,7 +19,7 @@ def product_detail(request, slugified_username, slug):
     product = get_object_or_404(Product, slug=slug, created_by=user.id, in_stock=True)
     return render(request, 'app/product-details.html', {'product': product})
 
-def category_products(request, slugified_username, slug):
+def a_user_category_products(request, slugified_username, slug):
     user = get_object_or_404(User, slugified_username=slugified_username)
     category = get_object_or_404(Category, slug=slug, created_by=user.id)
     category_products = Product.objects.filter(category=category, created_by=user.id, in_stock=True)
