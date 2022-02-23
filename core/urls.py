67e1a55ca3d5a14d8ff6530af3e324475fc33275
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-02-23 16:33:34 +0100

    commit

--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("app.urls", namespace="app")),
+    path("", include("app.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
