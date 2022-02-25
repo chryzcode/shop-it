5cf88db16ec9b91ecb60c306f60f287edab40164
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-02-25 21:26:55 +0100

    push

--- cart/views.py
+++ cart/views.py
@@ -38,7 +38,7 @@ def update_cart(request):
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('productid'))
         product_qty = int(request.POST.get('productqty'))
-        cart.update(product=product_id, product_qty=product_qty)
+        cart.update(product=product_id, qty=product_qty)
         cartqty = cart.__len__()
         carttotal = cart.get_total_price()
         response = JsonResponse({'qty':cartqty, 'subtotal':carttotal})
