df6ee2c4e19a9bfca9cf28afcb03b6fca93aec6d
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-05-30 23:15:12 +0100

    commit

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+import secrets
 
 # Create your models here.
+class Payment(models.Model):
+    amount = models.PositiveIntegerField()
+    ref = models.CharField(max_length=200)
+    email = models.EmailField()
+    verified = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_created')
+
+    def __str__(self):
+        return "payment" + ' ' + self.amount
+    
+    def save(self, *args, **kwargs) -> None:
+        while not self.ref:
+            ref = secrets.token_urlsafe(50)
+            objects_with_similar_ref = Payment.objects.filter(ref=ref)
+            if not objects_with_similar_ref:
+                self.ref = ref
+        super().save(*args, **kwargs)
+
+    def amount_value(self) -> int:
+        return self.amount *100
+
+
+          
