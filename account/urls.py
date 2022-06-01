7a78adbd7cca0d80cba33b4e8223a6a6a256e417
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-06-01 14:05:08 +0100

    push

--- account/urls.py
+++ account/urls.py
@@ -64,4 +64,5 @@ urlpatterns = [
     path("create-store/", views.create_store, name="create_store"),
     path("shipping-method/", views.shipping_method_list, name="shipping_method_list"),
     path("create/shipping-method/", views.add_shipping_method, name="add_shipping_method"),
+    path("edit/shipping-method/<str:pk>", views.edit_shipping_method, name="edit_shipping_method"),
 ]
