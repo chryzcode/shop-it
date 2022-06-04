a4b924b82beda7d9abe32c1434ee01bcc54f6152
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-06-04 11:51:30 +0100

    push

--- payment/views.py
+++ payment/views.py
@@ -51,9 +51,6 @@ def initiate_payment(request: HttpRequest, pk) -> HttpResponse:
 
             if payment.state == None:
                 return render(request, 'payment/initiate-payment.html', {'payment_form': payment_form, 'order': order, 'shipping_methods': shipping_methods, 'error': 'Please fill in all the fields'})
-
-            if payment.postcode == None:
-                return render(request, 'payment/initiate-payment.html', {'payment_form': payment_form, 'order': order, 'shipping_methods': shipping_methods, 'error': 'Please fill in all the fields'})
             
             if payment.city == None:
                 return render(request, 'payment/initiate-payment.html', {'payment_form': payment_form, 'order': order, 'shipping_methods': shipping_methods, 'error': 'Please fill in all the fields'})
