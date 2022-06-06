f2a56e2e2c5da8e9ad072f1a07eb6355ad4bdeca
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-06-06 11:04:29 +0100

    push

--- order/views.py
+++ order/views.py
@@ -15,30 +15,31 @@ def order(request, coupon_code):
     store = Store.objects.get(store_name=products.store)
     if not store.currency:
         error = "Store does'nt have a set currency for payment, drop a review for the store"
-        return redirect('cart_summary', store.slugified_store_name)
-    if request.user.is_authenticated:
-        user = request.user
+        return redirect('cart:cart_summary', store.slugified_store_name)
     else:
-        user = None
-    print("coupon_code", coupon_code)
-    if Coupon.objects.filter(code=coupon_code).exists():
-        coupon = Coupon.objects.get(code=coupon_code)
-        if request.user not in coupon.users.all():
-            coupon_percentage = coupon.percentage
-            amount = cart.get_grand_total(coupon_percentage)
+        if request.user.is_authenticated:
+            user = request.user
+        else:
+            user = None
+        print("coupon_code", coupon_code)
+        if Coupon.objects.filter(code=coupon_code).exists():
+            coupon = Coupon.objects.get(code=coupon_code)
+            if request.user not in coupon.users.all():
+                coupon_percentage = coupon.percentage
+                amount = cart.get_grand_total(coupon_percentage)
+            else:
+                amount = cart.get_total_price()
         else:
             amount = cart.get_total_price()
-    else:
-        amount = cart.get_total_price()
-    billing_status = False
-    quantity = cart.__len__()
-    order = Order.objects.create(
-        user=user,
-        store=store,
-        billing_status=billing_status,
-        amount=amount,
-        quantity=quantity,
-    )
-    order.set_product(products)
-    # cart.clear()
-    return redirect("payment:initiate_payment", order.id)
+        billing_status = False
+        quantity = cart.__len__()
+        order = Order.objects.create(
+            user=user,
+            store=store,
+            billing_status=billing_status,
+            amount=amount,
+            quantity=quantity,
+        )
+        order.set_product(products)
+        # cart.clear()
+        return redirect("payment:initiate_payment", order.id)
