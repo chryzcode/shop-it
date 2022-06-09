750813c48a6e0d004d1db053ad80bf2e1fa299f5
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-06-09 11:50:57 +0100

    push

--- app/views.py
+++ app/views.py
@@ -421,8 +421,10 @@ def store_orders(request):
     if request.user.store_staff == True:
         store = Store.objects.get(store_name=store_staff.objects.get(user=request.user).store)
     orders = Order.objects.filter(store=store)
-    print(orders)
-    payments = Payment.objects.filter(store=store.id)
+    if Payment.objects.filter(store=store.id, order__in=orders).exists():
+        payments = Payment.objects.get(store=store.id, order__in=orders)
+    else:
+        payments = None
     return render(request, "store/store-order.html", {"orders": orders, "payments": payments})
 
 @login_required(login_url="/account/login/")
