fb8c97c1e3e47549231427d4f7b4d494f36dfacb
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-06-16 09:04:01 +0100

    commit

--- account/views.py
+++ account/views.py
@@ -482,6 +482,8 @@ def bank_details(request):
             if form.is_valid():
                 bank_info = form.save(commit=False)
                 bank_info.store = store
+                #get bank code from name
+                bank_info.bank_code = all_banks[bank_info.bank_name]
                 bank_info.save()
                 Bank_Info.objects.exclude(pk=bank_info.pk).filter(store=store).delete()
                 return redirect("account:bank_details")
