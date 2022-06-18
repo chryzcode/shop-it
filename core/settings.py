ac4c42830daad8e4415770938b3d0bc22de79175
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-06-18 21:47:47 +0100

    commit

--- core/settings.py
+++ core/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "customer",
     "order",
     "payment",
+    "subscriptions",
 ]
 
 MIDDLEWARE = [
--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Subscription(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    monthly = models.BooleanField(default=False)
+    yearly = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
