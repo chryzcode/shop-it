d1b36f9b6a6244fd6d40bcf9acca6501570565f4
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-06-19 20:59:20 +0100

    push

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name="subscriptions"
 
 urlpatterns = [
-    
+    path("", views.initiate_subscription_payment, name="initiate_subscription_payment"),
+    path("<str:ref>/", views.verify_subscription_payment, name="verify_subscription_payment"),
 ]
