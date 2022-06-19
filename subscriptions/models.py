fb88a4d876d80d35902442ac5446de6bb0055cac
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-06-19 20:30:06 +0100

    commit

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -31,6 +31,20 @@ class Subscription(models.Model):
     def __str__(self):
         return self.name
 
+    def amount_value(self) -> int:
+        return self.amount * 100
+
+    def verify_payment(self):
+        paystack = Paystack()
+        status, result = paystack.verify_payment(self.ref, self.amount)
+        if status:
+            if result["amount"] / 100 == self.amount:
+                self.verified = True
+            self.save()
+        if self.verified:
+            return True
+        return False
+
     def save(self, *args, **kwargs) -> None:
         while not self.ref:
             ref = secrets.token_urlsafe(50)
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import redirect, render
 from django.http import HttpRequest, HttpResponse
 from .models import *
 from account.models import *
+from django.conf import settings
 
 # Create your views here.
 def initiate_subscription_payment(request: HttpRequest, pk) -> HttpResponse:
     if request.user.store_creator == True:
+        email = request.user.email
         store = Store.objects.get(store_name=request.user.store_name)
         subscription = Subscription.objects.get(pk=pk)
         if subscription.subscribers.filter(pk=store.pk).exists():
             return redirect("/")
         else:
-            return render(
-                request, "subscriptions/make-subscription-payments.html", 
-                {"subscription": subscription, "store": store}
+            return render(request, "subscriptions/make-subscription-payments.html", 
+                {"subscription": subscription, "store": store, "paystack_public_key":settings.PAYSTACK_PUBLIC_KEY, "email":email}
             )
     else:
         return redirect("/")
