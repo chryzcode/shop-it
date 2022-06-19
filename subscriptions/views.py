3229652b063a507d05cc46c2aab7a47ffa1fd3b1
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-06-19 19:25:44 +0100

    commit

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpRequest, HttpResponse
+from .models import *
+from account.models import *
 
 # Create your views here.
+def initiate_subscription_payment(request: HttpRequest, pk) -> HttpResponse:
+    if request.user.store_creator == True:
+        store = Store.objects.get(store_name=request.user.store_name)
+        subscription = Subscription.objects.get(pk=pk)
+        if subscription.subscribers.filter(pk=store.pk).exists():
+            return redirect("/")
+    else:
+        return redirect("/")
+
