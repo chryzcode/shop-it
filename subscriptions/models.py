99ab54e7774617d05925b4fec51c7dea542dd735
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-06-19 18:52:09 +0100

    commit

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -2,14 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+class SubscriptionDuration(models.Model):
+    duration = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.duration
+
+
 class Subscription(models.Model):
     name = models.CharField(max_length=100)
     price = models.PositiveIntegerField()
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    monthly = models.BooleanField(default=False)
-    yearly = models.BooleanField(default=False)
+    duration = models.ForeignKey(SubscriptionDuration, on_delete=models.CASCADE)
+    ref = models.CharField(max_length=200)
+    verified = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
