dd29e676ec625e119ce00fb628bb143556a15e61
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-06-28 08:24:45 +0100

    commit

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -9,12 +9,14 @@ document.onclick = function (e) {
   if (e.target.id == "a-nav-link") {
     nav_links.classList.remove("active");
     toogle.classList.remove("active");
+    store_side_nav.classList.remove("active");
   }
 };
 
 toogle.onclick = function () {
   toogle.classList.toggle("active");
-  nav_links.classList.toggle("active");
+  // nav_links.classList.toggle("active");
+  store_side_nav.classList.toggle("active");
 };
 
 
