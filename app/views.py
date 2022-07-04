2c48df3e24f7e7b595b66ce064ab6898f3d125ba
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-07-04 13:16:08 +0100

    commit

--- app/views.py
+++ app/views.py
@@ -300,7 +300,7 @@ def store_admin(request):
 
 def store(request, slugified_store_name):
     store = get_object_or_404(Store, slugified_store_name=slugified_store_name)
-    products = Product.objects.filter(store=store).order_by("-created")
+    products = Product.objects.filter(store=store).order_by("-created")[:12]
     return render(request, "store/store.html", {"store": store, "products": products, "slugified_store_name": slugified_store_name})
 
 
