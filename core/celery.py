787888d0201b1b008ab3605d9da057c3dd53a9ba
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-07-08 05:28:22 +0100

    push

--- core/celery.py
+++ core/celery.py
@@ -10,7 +10,7 @@ app = Celery('core')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 @app.task(bind=True)
@@ -30,4 +30,4 @@ app.conf.beat_schedule = {
 
 @app.task(bind=True)
 def debug_task(self):
-    print(f"Request: {self.request!r}")
+    print('Request: {0!r}'.format(self.request))
