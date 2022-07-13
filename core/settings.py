5752b1b56b112905bfdfdd62493cc9838e2ab5a7
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-07-13 20:59:26 +0100

    push

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['shopitapps.herokuapp.com', '127.0.0.1', 'localhost']
 
