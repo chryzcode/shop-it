64e8380d3ca2a505157aeef822fc866319fa4e10
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-07-31 22:30:04 +0100

    push

--- core/celery.py
+++ core/celery.py
@@ -1,31 +1,31 @@
-from __future__ import absolute_import
-import os
-from celery import Celery
-from django.conf import settings
-from celery.schedules import crontab
+# from __future__ import absolute_import
+# import os
+# from celery import Celery
+# from django.conf import settings
+# from celery.schedules import crontab
 
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-app = Celery('core')
+# # set the default Django settings module for the 'celery' program.
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+# app = Celery('core')
 
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
-
-@app.task(bind=True)
-def print_hello(self):
-    print('Hello World!')
+# # Using a string here means the worker will not have to
+# # pickle the object when using Windows.
+# app.config_from_object('django.conf:settings')
+# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
-app.conf.beat_schedule = {
-    'add-every-24-hour' : {
-        'task': 'hello',
-        'schedule': crontab(minute='*/1')
-    }
-}
+# @app.task(bind=True)
+# def debug_task(self):
+#     print('Request: {0!r}'.format(self.request))
+
+# @app.task(bind=True)
+# def print_hello(self):
+#     print('Hello World!')
+
+
+# app.conf.beat_schedule = {
+#     'add-every-24-hour' : {
+#         'task': 'hello',
+#         'schedule': crontab(minute='*/1')
+#     }
+# }
--- core/task.py
+++ core/task.py
@@ -1,19 +1,19 @@
-from core.celery import app
-from django.template.loader import render_to_string
-from django.utils import timezone
-from datetime import datetime, timedelta
+# from core.celery import app
+# from django.template.loader import render_to_string
+# from django.utils import timezone
+# from datetime import datetime, timedelta
 
 
-from celery.task.schedules import crontab
-from celery.decorators import periodic_task
+# from celery.task.schedules import crontab
+# from celery.decorators import periodic_task
 
-from celery.utils.log import get_task_logger
+# from celery.utils.log import get_task_logger
 
-logger = get_task_logger(__name__)
+# logger = get_task_logger(__name__)
 
-@periodic_task(run_every=crontab(minute='*/1'), name="hello", ignore_result=True)
-def hello():
-    logger.info("Hello World")
-    print('Hello World!')
-    return 'Hello World!'
+# @periodic_task(run_every=crontab(minute='*/1'), name="hello", ignore_result=True)
+# def hello():
+#     logger.info("Hello World")
+#     print('Hello World!')
+#     return 'Hello World!'
 
