e583e06a9ed5e0f00eb381d884311c02f05f39e9
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-08-30 17:06:05 +0100

    push

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["shopitapps.herokuapp.com", "127.0.0.1", "localhost", '.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ["shopitapps.herokuapp.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
