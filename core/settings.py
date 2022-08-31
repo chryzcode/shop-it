0183cf91d79b4a7d9235f8b3cb5a264410417a7c
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-08-31 12:22:49 +0100

    commit

--- core/settings.py
+++ core/settings.py
@@ -159,9 +159,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
