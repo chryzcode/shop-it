8dfbe9eb14f65d2bac8fdab92d9dcace3d4b7f25
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-09-09 15:05:32 +0100

    push

--- core/settings.py
+++ core/settings.py
@@ -161,8 +161,8 @@ USE_TZ = True
 
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
