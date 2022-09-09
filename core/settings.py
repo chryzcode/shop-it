2ff610e2ec0999f465631e0f74bb0eb212eb8557
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-09-09 10:15:09 +0100

    push

--- core/settings.py
+++ core/settings.py
@@ -161,9 +161,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app/static')]
+STATIC_URL = "/static/"
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 MEDIA_URL = "/shop-it/media/"
