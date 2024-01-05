d153ba9e4fbeb9b7bbf3d4c957b027fe1497469a
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-05 11:11:12 +0100

    init

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
 import dj_database_url
 from decouple import config
 
@@ -30,7 +29,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["shopitapps-33a59b3448c4.herokuapp.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["shopit-t07j.onrender.com", "127.0.0.1", "localhost"]
 
 
 
@@ -110,16 +109,16 @@ AUTH_USER_MODEL = "account.User"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-       "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST"),
-        "PORT": "5432",
-    }
-}
+# DATABASES = {
+#     "default": {
+#        "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": config("DB_NAME"),
+#         "USER": config("DB_USER"),
+#         "PASSWORD": config("DB_PASSWORD"),
+#         "HOST": config("DB_HOST"),
+#         "PORT": "5432",
+#     }
+# }
 
 
 
@@ -215,7 +214,5 @@ CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = "Africa/Lagos"
-
-django_heroku.settings(locals())
 
 
